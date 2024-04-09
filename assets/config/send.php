529ba81b6abb729b59a9207bc77612488e944c3b
Adnan Rahim <adnankaka.0092@gmail.com>
2024-04-09 23:51:31 +0500

    final update

--- assets/config/send.php
+++ assets/config/send.php
@@ -45,7 +45,7 @@ if (isset($_POST['add_client_btn'])) {
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
 
-    $mail->setfrom('adnankaka.0092@gmail.com');
+    $mail->setfrom($email);
 
     $mail->addAddress('syedamin.prohub@gmail.com');
 
@@ -75,5 +75,58 @@ if (isset($_POST['add_client_btn'])) {
     }
 }
 
+if (isset($_POST['c_sub_btn'])) {
+    $c_name = $_POST['c-name'];
+    $c_email = $_POST['c-email'];
+    $c_phone = $_POST['c-phone'];
+    $c_message = $_POST['c-message'];
+    $c_check = ($_POST['checkbox']) ? $_POST['checkbox'] : 0;
+    
+    $rapid_msg_query = "INSERT INTO rapid_message 
+    (c_name, c_email, c_phone, c_message, c_check) 
+    VALUES ('$c_name', '$c_email', '$c_phone', '$c_message', '$c_check')";
+
+    $rapid_msg_query_run = mysqli_query($conn, $rapid_msg_query);
+
+    if ($rapid_msg_query_run) {
+        $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'syedamin.prohub@gmail.com';
+    $mail->Password = 'bgmh szue ekts ijuu';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+
+    $mail->setfrom($c_email);
+
+    $mail->addAddress('syedamin.prohub@gmail.com');
+
+    $mail->isHTML(true);
+
+    $mail->Subject = 'Data from Contact-Us Form';
+    $mail->Body  = "Name: $c_name<br>
+                    Email: $c_email<br>
+                    Phone: $c_phone<br>
+                    Message: $c_message<br>
+                    Check-box: $c_check"
+    ;
+
+    $mail->send();
+
+    echo
+    "
+    <script>
+    document.location.href = '../../index.html'
+    alert('Your Message Sent Successfully');
+    </script>
+    ";
+    } else {
+        echo $mail->ErrorInfo;
+        header("location: ../../index.html");
+        echo "Error: " . $rapid_msg_query . "<br>" . $conn->error;
+    }
+}
+
 $conn->close();
-?>
