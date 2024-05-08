746a1d220bac7969685e332bb80a8d31a1adc436
Adnan Rahim <adnankaka.0092@gmail.com>
2024-05-08 20:41:57 +0500

    "Removed database connection and related code from send.php; updated PHPMailer configuration"
    This commit removes the code related to MySQL database connection from the send.php file, as the comments suggest. It also updates the

--- assets/config/send.php
+++ assets/config/send.php
@@ -1,26 +1,10 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
 require '../phpmailer/src/Exception.php';
 require '../phpmailer/src/PHPMailer.php';
 require '../phpmailer/src/SMTP.php';
 
-
-$host = "localhost";
-$username = "westernbook";
-$password = "pass0092word";
-$database = "westernbookpublishing";
-
-
-// Creating database connection
-$conn = mysqli_connect($host, $username, $password, $database);
-
-// Checking Database connection
-if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
-}
-// Get form data
 if (isset($_POST['add_client_btn'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -28,14 +12,7 @@ if (isset($_POST['add_client_btn'])) {
     $service_type = $_POST['service-type'];
     $message = $_POST['message'];
 
-    $booking_query = "INSERT INTO contact_us 
-    (name, email, phone, service_type, message) 
-    VALUES ('$name', '$email', '$phone', '$service_type', '$message')";
-
-    $booking_query_run = mysqli_query($conn, $booking_query);
-
-    if ($booking_query_run) {
-        $mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
 
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
@@ -52,7 +29,7 @@ if (isset($_POST['add_client_btn'])) {
     $mail->isHTML(true);
 
     $mail->Subject = 'Data from Contact-Us Form';
-    $mail->Body  = "Name: $name<br>
+    $mail->Body = "Name: $name<br>
                     Email: $email<br>
                     Phone: $phone<br>
                     Service Type: $service_type<br>
@@ -62,17 +39,16 @@ if (isset($_POST['add_client_btn'])) {
     $mail->send();
 
     echo
-    "
+        "
     <script>
     alert('Your Message Sent Successfully')
     document.location.href = '../../index.html';
     </script>
     ";
-    } else {
-        echo $mail->ErrorInfo;
-        header("location: ../../index.html");
-        echo "Error: " . $booking_query . "<br>" . $conn->error;
-    }
+} else {
+    echo $mail->ErrorInfo;
+    header("location: ../../index.html");
+    echo "Error: ". "<br>";
 }
 
 if (isset($_POST['c_sub_btn'])) {
@@ -81,42 +57,35 @@ if (isset($_POST['c_sub_btn'])) {
     $c_phone = $_POST['c-phone'];
     $c_message = $_POST['c-message'];
     $c_check = ($_POST['checkbox']) ? $_POST['checkbox'] : 0;
-    
-    $rapid_msg_query = "INSERT INTO rapid_message 
-    (c_name, c_email, c_phone, c_message, c_check) 
-    VALUES ('$c_name', '$c_email', '$c_phone', '$c_message', '$c_check')";
 
-    $rapid_msg_query_run = mysqli_query($conn, $rapid_msg_query);
-
-    if ($rapid_msg_query_run) {
         $mail = new PHPMailer(true);
 
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'syedamin.prohub@gmail.com';
-    $mail->Password = 'bgmh szue ekts ijuu';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'syedamin.prohub@gmail.com';
+        $mail->Password = 'bgmh szue ekts ijuu';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
 
-    $mail->setfrom($c_email);
+        $mail->setfrom($c_email);
 
-    $mail->addAddress('syedamin.prohub@gmail.com');
+        $mail->addAddress('syedamin.prohub@gmail.com');
 
-    $mail->isHTML(true);
+        $mail->isHTML(true);
 
-    $mail->Subject = 'Data from Contact-Us Form';
-    $mail->Body  = "Name: $c_name<br>
+        $mail->Subject = 'Data from Contact-Us Form';
+        $mail->Body = "Name: $c_name<br>
                     Email: $c_email<br>
                     Phone: $c_phone<br>
                     Message: $c_message<br>
                     Check-box: $c_check"
-    ;
+        ;
 
-    $mail->send();
+        $mail->send();
 
-    echo
-    "
+        echo
+            "
     <script>
     document.location.href = '../../index.html'
     alert('Your Message Sent Successfully');
@@ -124,9 +93,13 @@ if (isset($_POST['c_sub_btn'])) {
     ";
     } else {
         echo $mail->ErrorInfo;
-        header("location: ../../index.html");
-        echo "Error: " . $rapid_msg_query . "<br>" . $conn->error;
+        echo
+        "
+<script>
+document.location.href = '../../index.html'
+alert('Something Went Wrong');
+</script>
+";
     }
-}
 
-$conn->close();
+?>
